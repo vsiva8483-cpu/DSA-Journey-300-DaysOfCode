ac94ed2ca3604179bf09f33c8f46245c931533f1
Venkateswarlu vankaraboina <vsiva8483@gmail.com>
2026-07-18 19:21:24 +0530

    Add solution to find GCD of an array

---
+++ Leet-Code-Problems/Day 68 - Find Greatest Common Divisor of Array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findGCD(int[] nums) {
+        int min=nums[0];
+        int max=nums[0];
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]<min){
+                min=nums[i];
+            }
+            if(nums[i]>max){
+                max=nums[i];
+            }
+        }
+
+        int gcd = 1;
+
+        for(int i = 1; i <= min; i++) {
+            if(min % i == 0 && max % i == 0) {
+                gcd = i;
+            }
+        }
+
+        return gcd;
+    }
+}
+        
