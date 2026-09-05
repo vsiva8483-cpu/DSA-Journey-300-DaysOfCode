8fc73e9fa0fb7b5496d72cbb12253c706c310ed0
Venkateswarlu vankaraboina <230669206+vsiva8483-cpu@users.noreply.github.com>
2026-09-05 21:00:58 +0530

    Add Smallest Stable Index II solution

---
+++ Leet-Code-Problems/September/05-09-26 -> Smallest Stable Index II.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+
+        int n = nums.length;
+        int[] suffixMin = new int[n];
+
+        suffixMin[n -1 ] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = Math.min(nums[i], suffixMin[i + 1]);
+        }
+        int prefixMax = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+
+            prefixMax = Math.max(prefixMax, nums[i]);
+
+            int instability = prefixMax - suffixMin[i];
+
+            if (instability <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
