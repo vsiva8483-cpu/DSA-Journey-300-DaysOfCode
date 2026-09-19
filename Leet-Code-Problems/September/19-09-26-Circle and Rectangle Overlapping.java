6e932ddcb6ca898ad28743e987d6142099daebe2
Venkateswarlu vankaraboina <230669206+vsiva8483-cpu@users.noreply.github.com>
2026-09-19 14:40:26 +0530

    Add solution for circle and rectangle overlapping check

---
+++ Leet-Code-Problems/September/19-09-26-Circle and Rectangle Overlapping.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkOverlap(int radius, int xCenter, int yCenter,
+                                int x1, int y1, int x2, int y2) {
+
+        int x = Math.max(x1, Math.min(xCenter, x2));
+        int y = Math.max(y1, Math.min(yCenter, y2));
+
+        int dx = x - xCenter;
+        int dy = y - yCenter;
+
+        return dx * dx + dy * dy <= radius * radius;
+    }
+}
