e62f141c1f534ecc4a89cb5ad70fb378713b94b0
Venkateswarlu vankaraboina <230669206+vsiva8483-cpu@users.noreply.github.com>
2026-09-19 14:39:28 +0530

    Create 19-09-26-Min Cost To Make Two Strings Identical.java

---
+++ GFG/September/19-09-26-Min Cost To Make Two Strings Identical.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findMinCost(String s1, String s2, int costS1, int costS2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + costS1 + costS2;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        int totalCost = n * costS1 + m * costS2;
+
+        return totalCost - dp[n][m];
+    }
+}
